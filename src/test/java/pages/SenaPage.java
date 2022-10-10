a6988fb41fd5854d5cf9a3f11211eac365d35e07
Sena Yucedal <sena.ycdl61@gmail.com>
2022-10-10 19:21:51 +0300

    senacommit

---
+++ src/test/java/pages/SenaPage.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class SenaPage {
+    /*
+    1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Signup / Login' button
+5. Verify 'New User Signup!' is visible
+6. Enter name and already registered email address
+7. Click 'Signup' button
+8. Verify error 'Email Address already exist!' is visible
+     */
+}
---
+++ src/test/java/pages/TestCase07Sena.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class TestCase07Sena {
+
+    /*
+    1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Test Cases' button
+5. Verify user is navigated to test cases page successfully
+     */
+}
